0ed469d89157755ca9b05eef8b46b5c454a7a252
Vitorchx <vitorchx@outlook.com>
2023-03-29 20:41:42 -0300

    xx

--- tipo-variaveis/src/tiposVariaveis.java
+++ tipo-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,14 @@
-public class tiposVariaveis{
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor de a: ");
+        int a = scanner.nextInt();
+
+        System.out.println("valor é: " + a);
+
     }
 }
+
